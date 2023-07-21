a851b2303faadc30a7a9d12f066c29bdaa940b45
Norbert Takacs <norbert.takacs@postnord.com>
2023-07-21 17:32:28 +0200

    changed nuxt config to not ask for telemetry

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,52 +1,53 @@
-const colors = require('vuetify/es5/util/colors').default
+const colors = require("vuetify/es5/util/colors").default;
 const URLS = {
-  LOGIN: '/api/login',
-  GET_SERVERS: '/api/getServers',
-  ARRAY_STATUS: '/api/arrayStatus',
-  SERVER_STATUS: '/api/serverStatus',
-  VM_STATUS: '/api/vmStatus',
-  USB_ATTACH: '/api/usbAttach',
-  PCI_ATTACH: '/api/pciAttach',
-  GPU_SWAP: '/api/gpuSwap',
-  VM_EDIT: '/api/editVM',
-  VM_CREATE: '/api/createVM',
-  DOCKER_STATUS: '/api/dockerStatus',
-  MQTT_DEVICE_CHANGE: '/api/mqttDevices',
-  DELETE_SERVER: '/api/deleteServer',
-  PROXY_IMAGE: '/state',
-  PROXY_IMAGE_VM: '/plugins'
+  LOGIN: "/api/login",
+  GET_SERVERS: "/api/getServers",
+  ARRAY_STATUS: "/api/arrayStatus",
+  SERVER_STATUS: "/api/serverStatus",
+  VM_STATUS: "/api/vmStatus",
+  USB_ATTACH: "/api/usbAttach",
+  PCI_ATTACH: "/api/pciAttach",
+  GPU_SWAP: "/api/gpuSwap",
+  VM_EDIT: "/api/editVM",
+  VM_CREATE: "/api/createVM",
+  DOCKER_STATUS: "/api/dockerStatus",
+  MQTT_DEVICE_CHANGE: "/api/mqttDevices",
+  DELETE_SERVER: "/api/deleteServer",
+  PROXY_IMAGE: "/state",
+  PROXY_IMAGE_VM: "/plugins"
 };
 
 module.exports = {
-  mode: 'universal',
+  telemetry: false,
+  mode: "universal",
   /*
    ** Headers of the page
    */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: "%s - " + process.env.npm_package_name,
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
       }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
       {
-        rel: 'stylesheet',
+        rel: "stylesheet",
         href:
-          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
+          "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons"
       }
     ]
   },
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: { color: "#fff" },
   /*
    ** Global CSS
    */
@@ -59,12 +60,12 @@ module.exports = {
    ** Nuxt.js modules
    */
   modules: [
-    '@nuxtjs/vuetify',
+    "@nuxtjs/vuetify",
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
-    '@nuxtjs/eslint-module',
-    '~/mqtt'
+    "@nuxtjs/axios",
+    "@nuxtjs/pwa",
+    "@nuxtjs/eslint-module",
+    "~/mqtt"
   ],
   /*
    ** Axios module configuration
@@ -74,21 +75,21 @@ module.exports = {
 
   serverMiddleware: [
     // Will register file from project api directory to handle /api/* requires
-    {path: URLS.LOGIN, handler: '~/api/login.js'},
-    {path: URLS.GET_SERVERS, handler: '~/api/getServers.js'},
-    {path: URLS.VM_STATUS, handler: '~/api/changeVMStatus.js'},
-    {path: URLS.DOCKER_STATUS, handler: '~/api/changeDockerStatus.js'},
-    {path: URLS.ARRAY_STATUS, handler: '~/api/changeArrayStatus.js'},
-    {path: URLS.SERVER_STATUS, handler: '~/api/changeServerStatus.js'},
-    {path: URLS.USB_ATTACH, handler: '~/api/usbAttach.js'},
-    {path: URLS.PCI_ATTACH, handler: '~/api/pciAttach.js'},
-    {path: URLS.GPU_SWAP, handler: '~/api/gpuSwap.js'},
-    {path: URLS.VM_EDIT, handler: '~/api/editVM.js'},
-    {path: URLS.VM_CREATE, handler: '~/api/createVM.js'},
-    {path: URLS.MQTT_DEVICE_CHANGE, handler: '~/api/mqttDevices.js'},
-    {path: URLS.DELETE_SERVER, handler: '~/api/deleteServer.js'},
-    {path: URLS.PROXY_IMAGE, handler: '~/api/proxyImage.js'},
-    {path: URLS.PROXY_IMAGE_VM, handler: '~/api/proxyImage.js'}
+    { path: URLS.LOGIN, handler: "~/api/login.js" },
+    { path: URLS.GET_SERVERS, handler: "~/api/getServers.js" },
+    { path: URLS.VM_STATUS, handler: "~/api/changeVMStatus.js" },
+    { path: URLS.DOCKER_STATUS, handler: "~/api/changeDockerStatus.js" },
+    { path: URLS.ARRAY_STATUS, handler: "~/api/changeArrayStatus.js" },
+    { path: URLS.SERVER_STATUS, handler: "~/api/changeServerStatus.js" },
+    { path: URLS.USB_ATTACH, handler: "~/api/usbAttach.js" },
+    { path: URLS.PCI_ATTACH, handler: "~/api/pciAttach.js" },
+    { path: URLS.GPU_SWAP, handler: "~/api/gpuSwap.js" },
+    { path: URLS.VM_EDIT, handler: "~/api/editVM.js" },
+    { path: URLS.VM_CREATE, handler: "~/api/createVM.js" },
+    { path: URLS.MQTT_DEVICE_CHANGE, handler: "~/api/mqttDevices.js" },
+    { path: URLS.DELETE_SERVER, handler: "~/api/deleteServer.js" },
+    { path: URLS.PROXY_IMAGE, handler: "~/api/proxyImage.js" },
+    { path: URLS.PROXY_IMAGE_VM, handler: "~/api/proxyImage.js" }
   ],
   /*
    ** vuetify module configuration
@@ -114,4 +115,4 @@ module.exports = {
      */
     extend(config, ctx) {}
   }
-}
+};
