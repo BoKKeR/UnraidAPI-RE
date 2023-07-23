ae173746782aa64168a8af7e226c85b26e04e2d7
Norbert Takacs <norbert.takacs@postnord.com>
2023-07-23 14:55:32 +0200

    removed log

--- utils/extractUsbDetails.ts
+++ utils/extractUsbDetails.ts
@@ -2,10 +2,6 @@ import { extractValue } from "./extractValue";
 
 export const extractUsbDetails = (data) => {
   let row = extractValue(data, '<label for="usb', "</label>");
-  console.log({
-    id: extractValue(row, 'value="', '"'),
-    name: extractValue(row, "/> ", " (")
-  });
 
   const nameSplits = extractValue(row, "/> ", " (").split(">");
   return {
