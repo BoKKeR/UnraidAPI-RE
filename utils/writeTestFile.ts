305dda554163694a0b1d08e3416a60df6e7e5bbe
Norbert Takacs <norbert.takacs@postnord.com>
2023-07-31 14:03:34 +0200

    change console log

--- utils/writeTestFile.ts
+++ utils/writeTestFile.ts
@@ -4,15 +4,16 @@ const writeTestFile = (data: string, name: string) => {
   if (process.env.WRITE_HTML_OUTPUT === "true") {
     const path = `config/html_output`;
     const file = `${name}.html`;
+    const fullPath = `${path}/${file}`;
     fs.mkdir(path, { recursive: true }, function(err) {
       if (err) {
         console.log(err);
       } else {
-        fs.writeFile(`${path}/${file}`, data, (err) => {
+        fs.writeFile(fullPath, data, (err) => {
           if (err) {
             console.error(err);
           }
-          console.log(`${path} written successfully`);
+          console.log(`${fullPath} written successfully`);
         });
       }
     });
