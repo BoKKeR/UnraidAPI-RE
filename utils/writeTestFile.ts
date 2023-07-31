6989491dd6abc58c2f12757342c9fe7d8bc9ac6b
Norbert Takacs <norbert.takacs@postnord.com>
2023-07-31 13:47:15 +0200

    changed folder

--- utils/writeTestFile.ts
+++ utils/writeTestFile.ts
@@ -2,12 +2,18 @@ import fs from "fs";
 
 const writeTestFile = (data: string, name: string) => {
   if (process.env.WRITE_HTML_OUTPUT === "true") {
-    const path = `config/${name}.html`;
-    fs.writeFile(path, data, (err) => {
+    const path = `config/html_output/${name}.html`;
+    fs.mkdir(path, { recursive: true }, function(err) {
       if (err) {
-        console.error(err);
+        console.log(err);
+      } else {
+        fs.writeFile(path, data, (err) => {
+          if (err) {
+            console.error(err);
+          }
+          console.log(`${path} written successfully`);
+        });
       }
-      console.log(`${path} written successfully`);
     });
   }
 };
