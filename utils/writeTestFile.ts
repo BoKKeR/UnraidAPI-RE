27126182e0ea0855b262dcc54c36d308f9ec0371
Norbert Takacs <norbert.takacs@postnord.com>
2023-07-31 14:00:03 +0200

    changed path

--- utils/writeTestFile.ts
+++ utils/writeTestFile.ts
@@ -2,12 +2,13 @@ import fs from "fs";
 
 const writeTestFile = (data: string, name: string) => {
   if (process.env.WRITE_HTML_OUTPUT === "true") {
-    const path = `config/html_output/${name}.html`;
+    const path = `config/html_output`;
+    const file = `${name}.html`;
     fs.mkdir(path, { recursive: true }, function(err) {
       if (err) {
         console.log(err);
       } else {
-        fs.writeFile(path, data, (err) => {
+        fs.writeFile(`${path}/${file}`, data, (err) => {
           if (err) {
             console.error(err);
           }
