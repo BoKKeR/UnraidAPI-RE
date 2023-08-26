ece98084eda1a20a8ba47ea102e9753759d1d9ca
Norbert Takacs <norbert.takacs@postnord.com>
2023-08-26 07:31:47 +0200

    replaced function

--- utils/Unraid.ts
+++ utils/Unraid.ts
@@ -24,6 +24,7 @@ import extractCsrfToken from "./extractCsrfToken";
 import { enableVmFetching } from "./enableVmFetching";
 import { RootServerJSONConfig, Vm } from "~/types/server";
 import { enableDockerFetching } from "./enableDockerFetching";
+import extractUSBData from "./extractUsbData";
 
 const fetch = require("node-fetch");
 
@@ -1276,42 +1277,6 @@ function extractShareData(data: string) {
     data = data.replace("<td>Unraid Share:</td>", "");
   }
   return shares;
-}
-
-function extractUSBData(data: string, vmObject, ip: string) {
-  let usbs: UsbData[] = [];
-  let usbInfo = extractValue(data, "<td>USB Devices:</td>", "</td>");
-  while (usbInfo.includes('value="')) {
-    let row = extractValue(usbInfo, 'value="', " (");
-    let usb = {} as UsbData;
-    if (row.includes("checked")) {
-      usb.checked = true;
-    }
-    usb.id = row.substring(0, row.indexOf('"'));
-    usb.name = row.substring(row.indexOf("/") + 3);
-    usb.connected = true;
-    usbs.push(usb);
-    usbInfo = usbInfo.replace('value="', "");
-  }
-  let rawdata = fs.readFileSync("config/servers.json");
-  let servers = JSON.parse(rawdata.toString());
-  let oldUsbs: UsbData[] = [];
-  try {
-    if (servers[ip].vm?.details[vmObject.id]?.edit) {
-      oldUsbs = servers[ip].vm.details[vmObject.id].edit.usbs;
-    }
-  } catch (error) {
-    console.log("error getting old Usb devices for VM");
-  }
-  if (oldUsbs && oldUsbs.length > usbs.length) {
-    oldUsbs.forEach((usb) => {
-      if (usbs.filter((usbInner) => usbInner.id === usb.id).length === 0) {
-        usb.connected = false;
-        usbs.push(usb);
-      }
-    });
-  }
-  return usbs;
 }
 
 function extractPCIData(data: string) {
