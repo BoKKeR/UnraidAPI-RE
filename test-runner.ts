9ee08aceb42bba2105a964f7b6446844b63e30e8
Norbert Takacs <norbert.takacs@postnord.com>
2024-02-01 18:41:48 +0100

    fixed test

--- test-runner.ts
+++ test-runner.ts
@@ -5,28 +5,43 @@ import { setTimeout } from "timers/promises";
 
 const directoryPath = "./unraid-versions";
 
+const testFolderParam = process.argv[2];
+
 const main = async () => {
   // Read the directory
   const files = fs.readdirSync(directoryPath);
 
-  // Assuming each folder name is a valid environment variable
-  for (const folder of files) {
-    // Set environment variable based on folder name
-    process.env.UNRAID_VERSION = folder;
-    console.log(`Set environment variable ${folder} to ${folder}`);
+  if (testFolderParam) {
+    testVersion(testFolderParam);
+  } else {
+    // Assuming each folder name is a valid environment variable
+    for (const folder of files) {
+      // Set environment variable based on folder name
+      testVersion(folder);
+    }
+  }
+};
 
-    // Execute Jest for each folder
-    const jestProcess = child_process.spawn("./node_modules/.bin/jest", [], {
+const testVersion = async (folder: string) => {
+  // Set environment variable based on folder name
+  process.env.UNRAID_VERSION = folder;
+  console.log(`Set environment variable ${folder} to ${folder}`);
+
+  // Execute Jest for each folder
+  const jestProcess = child_process.spawn(
+    "./node_modules/.bin/jest --coverage=false",
+    [],
+    {
       stdio: "inherit",
       shell: true,
       env: { ...process.env } // Pass the current environment along with the new variable
-    });
-    await setTimeout(5000);
+    }
+  );
+  await setTimeout(5000);
 
-    jestProcess.on("exit", (code, signal) => {
-      console.log(`Jest process for ${folder} exited with code ${code}`);
-    });
-  }
+  jestProcess.on("exit", (code, signal) => {
+    console.log(`Jest process for ${folder} exited with code ${code}`);
+  });
 };
 
 main();
--- utils/extractUsbDetails.test.ts
+++ utils/extractUsbDetails.test.ts
@@ -1,15 +1,34 @@
 import fs from "fs";
 import { extractUsbDetails } from "./extractUsbDetails";
 
-const result = {
-  id: "05e3:0749",
-  name: "Genesys Logic SD Card Reader and Writer"
-};
+const result = [
+  {
+    id: "05e3:0749",
+    name: "Genesys Logic SD Card Reader and Writer"
+  },
+  {
+    id: "0627:0001",
+    name: "Adomax Technology Co. QEMU USB Tablet"
+  }
+];
 
 test("Tests against extracted version html for usb", () => {
   const buffer = fs.readFileSync(
     `./unraid-versions/${process.env.UNRAID_VERSION}/updateVM.html`
   );
-  const input = extractUsbDetails(buffer.toString());
-  expect(input).toMatchObject(result);
+  const inputObj = extractUsbDetails(buffer.toString());
+  expect(result.some((resultObj) => isEqual(inputObj, resultObj))).toBeTruthy();
 });
+
+function isEqual(objA: any, objB: any): boolean {
+  const keysA = Object.keys(objA);
+  const keysB = Object.keys(objB);
+
+  if (keysA.length !== keysB.length) return false;
+
+  for (const key of keysA) {
+    if (objA[key] !== objB[key]) return false;
+  }
+
+  return true;
+}
